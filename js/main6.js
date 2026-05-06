065373b3f218da1b03b6a3ec5ce29e331c5a4eb0
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-05-06 21:03:30 +0300

    Edit işlemlerimizi tutacağımız değişkeni oluşturdum

--- js/main6.js
+++ js/main6.js
@@ -1,5 +1,6 @@
 let habit = [];
 let selectedCategory = "Hepsi";
+let editID = null;
 
 window.onload = function () {
   const data = localStorage.getItem("habit");
@@ -12,6 +13,7 @@ window.onload = function () {
     selectedCategory = e.target.value;
     renderHabit();
   };
+
   renderHabit();
 };
 
@@ -19,74 +21,76 @@ function generateID() {
   return Date.now();
 }
 
-function addHabit(){
-    const name = document.getElementById("nameInput");
-    const type = document.getElementById("typeInput");
-    const category = document.getElementById("categoryInput");
+function addHabit() {
+  const nameInput = document.getElementById("nameInput");
+  const typeInput = document.getElementById("typeInput");
+  const categoryInput = document.getElementById("category");
 
-    name = nameInput.value;
-    type = typeInput.value;
-    category = categoryInput.value;
+  const name = nameInput.value;
+  const type = typeInput.value;
+  const category = categoryInput.value;
 
-    if(name === " " || type === " ") return;
+  if (name === "" || type === "") return;
+
 
     const newItem = {
-        id:generateID(),
-        name:name,
-        type:type,
-        category:category,
-        completed: false,
-    }
+      id: generateID(),
+      name: name,
+      type: type,
+      category: category,
+      completed: false,
+    };
 
     habit.push(newItem);
-    localStorage.setItem("habit", JSON.stringify(habit));
 
-    renderHabit();
+  localStorage.setItem("habit", JSON.stringify(habit));
 
-    nameInput = "";
-    typeInput = "";
+  renderHabit();
 
+  nameInput.value = "";
+  typeInput.value = "";
 }
 
-function renderHabit(){
-    const list = document.getElementById("habitList");
-    list.innerHTML = "";
+function renderHabit() {
+  const list = document.getElementById("habitList");
+  list.innerHTML = "";
 
-    let filteredHabit = habit;
+  let filteredHabit = habit;
 
-    if(selectedCategory !== "Hepsi"){
-        filteredHabit = habit.filter(item => item.category === selectedCategory);
+  if (selectedCategory !== "Hepsi") {
+    filteredHabit = habit.filter((item) => item.category === selectedCategory);
+  }
+
+
+  filteredHabit.forEach(function (item) {
+    const li = document.createElement("li");
+    li.textContent = item.name + " " + item.type + " " + item.category;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = item.completed;
+
+    checkbox.onchange = function () {
+      item.completed = !item.completed;
+      localStorage.setItem("habit", JSON.stringify(habit));
+      renderHabit();
+    };
+
+    if (item.completed) {
+      li.style.textDecoration = "line-through";
     }
 
-    filteredHabit.forEach(function(item){
-        const li = document.createElement("li");
-        li.textContent = item.name + " " + item.type + " " + item.category;
+    const btn = document.createElement("button");
+    btn.textContent = "Sil";
 
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.checked = item.completed;
+    btn.onclick = function () {
+      habit = habit.filter((h) => h.id !== item.id);
+      localStorage.setItem("habit", JSON.stringify(habit));
+      renderHabit();
+    };
 
-        checkbox.onchange = function(){
-            item.completed = !item.completed;
-            localStorage.setItem("habit", JSON.stringify(habit));
-            renderHabit();
-        }
-
-        if(item.completed){
-            li.style.textDecoration = "line-through";
-        }
-
-        const btn = document.createElement("button");
-        btn.textContent = "Sil";
-
-        btn.onclick = function(){
-            filteredHabit = habit.filter((h) => h.id !== item.id);
-            localStorage.setItem("habit", JSON.stringify(habit));
-            renderHabit();
-        }
-
-        li.appendChild(checkbox);
-        li.appendChild(btn);
-        list.appendChild(li);
-    })
+    li.appendChild(checkbox);
+    li.appendChild(btn);
+    list.appendChild(li);
+  });
 }
