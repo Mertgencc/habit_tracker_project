7cf643c4c88948513e6639516b4880ef736a7a23
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-05-06 03:11:10 +0300

    Sayfaya verileri ekleme fonksiyonu hazırlandı

--- js/main6.js
+++ js/main6.js
@@ -1,16 +1,49 @@
 let habit = [];
 let selectedCategory = "Hepsi";
 
-window.onload = function(){
-    const data = localStorage.getItem("habit");
+window.onload = function () {
+  const data = localStorage.getItem("habit");
 
-    if(data){
-        habit = JSON.parse(data);
-    }
+  if (data) {
+    habit = JSON.parse(data);
+  }
 
-    document.getElementById("category").onchange = function(e){
-        selectedCategory = e.target.value;
-        renderHabit();
-    }
+  document.getElementById("category").onchange = function (e) {
+    selectedCategory = e.target.value;
     renderHabit();
+  };
+  renderHabit();
+};
+
+function generateID() {
+  return Date.now();
+}
+
+function addHabit(){
+    const name = document.getElementById("nameInput");
+    const type = document.getElementById("typeInput");
+    const category = document.getElementById("categoryInput");
+
+    name = nameInput.value;
+    type = typeInput.value;
+    category = categoryInput.value;
+
+    if(name === " " || type === " ");
+
+    const newItem = {
+        id:generateID(),
+        name:name,
+        type:type,
+        category:category,
+        completed: false,
+    }
+
+    habir.push(newItem);
+    localStorage.setItem("habit", JSON.stringify(habit));
+
+    renderHabit();
+
+    nameInput.value = "";
+    typeInput.value = "";
+    
 }
